941dd55e846090b09bc9285ca2512889ee8945aa
Rahul Raveendran V P <rahul.pmna@gmail.com>
2012-01-17 12:05:27 +0530

    No bugs here :) all four functions works properly :)

--- src/class.confirmation.php
+++ src/class.confirmation.php
@@ -38,34 +38,37 @@
 						'".$this->phoneNo."')";
 			dbquery($query);
 		}
-#		
-#		/* This fuction returns all the rows by taking the team ID*/
-#		function viewConfirmation($teamId) {
-#			$qry = "select * from confirmation where teamid='".$teamId."'";
-#			$res = dbquery($qry);
-#			$this->resourcevar = (resource2array($res));
-#			$res = dbquery($qry);
-#			$rec = pg_fetch_row($res);
-#			$this->teamId = $rec[0];
-#			$this->teamName = $rec[1];
-#			$this->collegeName = $rec[2];
-#			$this->teamLeader = $rec[3];
-#			$this->participant1 = $rec[4];
-#			$this->participant2 = $rec[5];
-#			$this->participant3 = $rec[6];
-#			$this->participant4 = $rec[7];
-#			$this->phoneNo = $rec[8];
-#		}
-#		
-#		/* function to retuen the resourcevar after searching in the table for contents that matches.*/
-#		public static function searchTeam($arg) {
-#			$arg='%'.$arg.'%';
-#			$qry = "select * from confirmation where 
-#						(teamname like '".$arg."') OR 
-#						(teamid like '".$arg."') OR 
-#						(teamleader like '".$arg."')";
-#						
-#			return (resource2array(dbquery($qry)));
-#		}
+		
+		/* This fuction returns all the rows by taking the team ID*/
+		function viewConfirmation($teamId) {
+			$qry = "select * from confirmation where teamid='".$teamId."'";
+			$res = dbquery($qry);
+			$this->resourcevar = (resource2array($res));
+			$res = dbquery($qry);
+			$rec = pg_fetch_row($res);
+			$this->teamId = $rec[0];
+			$this->teamName = $rec[1];
+			$this->collegeName = $rec[2];
+			$this->teamLeader = $rec[3];
+			$this->participant1 = $rec[4];
+			$this->participant2 = $rec[5];
+			$this->participant3 = $rec[6];
+			$this->participant4 = $rec[7];
+			$this->phoneNo = $rec[8];
+		}
+		public function getResourceArray() {
+			return $this->resourcevar;
+		}
+		
+		/* function to retuen the resourcevar after searching in the table for contents that matches.*/
+		public static function searchConfirmedTeam($arg) {
+			$arg='%'.$arg.'%';
+			$qry = "select * from confirmation where 
+						(teamname like '".$arg."') OR 
+						(teamid like '".$arg."') OR 
+						(teamleader like '".$arg."')";
+						
+			return (resource2array(dbquery($qry)));
+		}
 	}
 ?>
