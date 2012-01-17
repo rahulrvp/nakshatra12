6d2e70d6a37f9e36cddbfca9a55603302400d7b6
Rahul Raveendran V P <rahul.pmna@gmail.com>
2012-01-17 11:57:17 +0530

    Bugs cleared in confirmTeam() function

--- src/class.confirmation.php
+++ src/class.confirmation.php
@@ -26,7 +26,7 @@
 			$this->participant3 = pg_escape_string($cParticipant3);
 			$this->participant4 = pg_escape_string($cParticipant4);
 			$this->phoneNo = pg_escape_string($cPhoneNo);
-			$query = "Insert Into confirmation values (
+			$query = "insert into confirmation values (
 						'".$this->teamId."',
 						'".$this->teamName."',
 						'".$this->collegeName."',
@@ -35,37 +35,37 @@
 						'".$this->participant2."',
 						'".$this->participant3."',
 						'".$this->participant4."',
-						'".$this->phoneNo."');"
+						'".$this->phoneNo."')";
 			dbquery($query);
 		}
-		
-		/* This fuction returns all the rows by taking the team ID*/
-		function viewConfirmation($teamId) {
-			$qry = "select * from confirmation where teamid='".$teamId."'";
-			$res = dbquery($qry);
-			$this->resourcevar = (resource2array($res));
-			$res = dbquery($qry);
-			$rec = pg_fetch_row($res);
-			$this->teamId = $rec[0];
-			$this->teamName = $rec[1];
-			$this->collegeName = $rec[2];
-			$this->teamLeader = $rec[3];
-			$this->participant1 = $rec[4];
-			$this->participant2 = $rec[5];
-			$this->participant3 = $rec[6];
-			$this->participant4 = $rec[7];
-			$this->phoneNo = $rec[8];
-		}
-		
-		/* function to retuen the resourcevar after searching in the table for contents that matches.*/
-		public static function searchTeam($arg) {
-			$arg='%'.$arg.'%';
-			$qry = "select * from confirmation where 
-						(teamname like '".$arg."') OR 
-						(teamid like '".$arg."') OR 
-						(teamleader like '".$arg."')";
-						
-			return (resource2array(dbquery($qry)));
-		}
+#		
+#		/* This fuction returns all the rows by taking the team ID*/
+#		function viewConfirmation($teamId) {
+#			$qry = "select * from confirmation where teamid='".$teamId."'";
+#			$res = dbquery($qry);
+#			$this->resourcevar = (resource2array($res));
+#			$res = dbquery($qry);
+#			$rec = pg_fetch_row($res);
+#			$this->teamId = $rec[0];
+#			$this->teamName = $rec[1];
+#			$this->collegeName = $rec[2];
+#			$this->teamLeader = $rec[3];
+#			$this->participant1 = $rec[4];
+#			$this->participant2 = $rec[5];
+#			$this->participant3 = $rec[6];
+#			$this->participant4 = $rec[7];
+#			$this->phoneNo = $rec[8];
+#		}
+#		
+#		/* function to retuen the resourcevar after searching in the table for contents that matches.*/
+#		public static function searchTeam($arg) {
+#			$arg='%'.$arg.'%';
+#			$qry = "select * from confirmation where 
+#						(teamname like '".$arg."') OR 
+#						(teamid like '".$arg."') OR 
+#						(teamleader like '".$arg."')";
+#						
+#			return (resource2array(dbquery($qry)));
+#		}
 	}
 ?>
