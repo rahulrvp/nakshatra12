933e7c259c67e79e19bfa54a4b730c0f7d3000c5
Rahul Raveendran V P <rahul.pmna@gmail.com>
2012-01-17 11:51:38 +0530

    Bugs cleared.. :) All functions works properly..!

--- src/class.registration.php
+++ src/class.registration.php
@@ -35,32 +35,36 @@
 						'".$this->password."')";
 			dbquery($query);
 		}
-#		
-#		/* This fuction returns all the rows by taking the team ID*/
-#		function viewRegistration($teamId) {
-#			$qry = "select * from registration where teamid='".$teamId."'";
-#			$res = dbquery($qry);
-#			$this->resourcevar = (resource2array($res));
-#			$res = dbquery($qry);
-#			$rec = pg_fetch_row($res);
-#			$this->teamId = $rec[0];
-#			$this->teamName = $rec[1];
-#			$this->collegeName = $rec[2];
-#			$this->teamLeader = $rec[3];
-#			$this->emailId = $rec[4];
-#			$this->phoneNo = $rec[5];
-#			$this->password = $rec[6];
-#		}
-#		
-#		/* function to retuen the resourcevar after searching in the table for contents that matches.*/
-#		public static function searchRegisteredTeam($arg) {
-#			$arg='%'.$arg.'%';
-#			$qry = "select * from registration where 
-#						(teamname like '".$arg."') OR 
-#						(teamid like '".$arg."') OR 
-#						(teamleader like '".$arg."')";
-#						
-#			return (resource2array(dbquery($qry)));
-#		}
+
+		/* This fuction returns all the rows by taking the team ID*/
+		function viewRegistration($teamId) {
+			$qry = "select * from registration where teamid = '".$teamId."'";
+			$res = dbquery($qry);
+			$this->resourcevar = (resource2array($res));
+			$res = dbquery($qry);
+			$rec = pg_fetch_row($res);
+			$this->teamId = $rec[0];
+			$this->teamName = $rec[1];
+			$this->collegeName = $rec[2];
+			$this->teamLeader = $rec[3];
+			$this->emailId = $rec[4];
+			$this->phoneNo = $rec[5];
+			$this->password = $rec[6];
+		}
+		
+		public function getResourceArray() {
+			return $this->resourcevar;
+		}
+		
+		/* function to retuen the resourcevar after searching in the table for contents that matches.*/
+		public static function searchRegisteredTeam($arg) {
+			$arg='%'.$arg.'%';
+			$qry = "select * from registration where 
+						(teamname like '".$arg."') OR 
+						(teamid like '".$arg."') OR 
+						(teamleader like '".$arg."')";
+						
+			return (resource2array(dbquery($qry)));
+		}
 	}
 ?>
